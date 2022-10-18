e8a4a57ffec6e6bcaa368240778a3a463ec5104c
ramosw1993 <ramosw1993@gmail.com>
2022-10-17 23:48:37 -0400

    update port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const api = require("./routes/api");
 const html = require("./routes/html");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
