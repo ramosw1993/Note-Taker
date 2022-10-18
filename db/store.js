6b859d56e25b17bf23472490a1fbc534091f7542
ramosw1993 <ramosw1993@gmail.com>
2022-10-17 22:14:17 -0400

    update server.js

--- db/store.js
+++ db/store.js
@@ -1,7 +1,6 @@
 const util = require("util");
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
-// const uuidv4 = require("uuid/v4");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
